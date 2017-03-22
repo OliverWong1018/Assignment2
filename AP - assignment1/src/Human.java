a114e3c1e30ae793861d587bf54572babe73e64d
JINZE WANG <s3577346@student.rmit.edu.au>
2017-03-23 10:59:43 +1100

    updated

--- AP - assignment1/src/Human.java
+++ AP - assignment1/src/Human.java
@@ -1,10 +1,8 @@
 
-public class Swimmer {
+public class Human {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2233");
-		System.out.println("445");
 
 	}
 
