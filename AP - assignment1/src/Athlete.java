047e8b85d7a4114c5cbed226a8e0532ac6abe6d4
JINZE WANG <s3577346@student.rmit.edu.au>
2017-03-23 19:56:18 +1100

    updated

--- AP - assignment1/src/Athlete.java
+++ AP - assignment1/src/Athlete.java
@@ -1,5 +1,10 @@
 
-public class Athlete {
+public class Athlete extends Participant{
+
+	public Athlete(String ID, String name, int age, String state) {
+		super(ID, name, age, state);
+		// TODO Auto-generated constructor stub
+	}
 
 	
 
