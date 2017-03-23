bdaccae9c51ff36aa1c76303fffc8ce6a4620520
JINZE WANG <s3577346@student.rmit.edu.au>
2017-03-23 19:58:19 +1100

    updated

---
+++ AP - assignment1/src/Official.java
@@ -0,0 +1,10 @@
+
+public class Official extends Participant {
+
+	public Official(String ID, String name, int age, String state) {
+		super(ID, name, age, state);
+		// TODO Auto-generated constructor stub
+	} 
+
+	
+}
