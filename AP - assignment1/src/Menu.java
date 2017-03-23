eccaaaa7ba7dafa33df46f6cef51a35d487e32f7
JINZE WANG <s3577346@student.rmit.edu.au>
2017-03-23 15:08:03 +1100

    updated

--- AP - assignment1/src/Menu.java
+++ AP - assignment1/src/Menu.java
@@ -1,11 +1,61 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Menu {
-
-	public static void main(String[] args) {
-		private void mainMune(){
-			System.out.print("Ozlympic Game");
+	
+	Scanner sc = new Scanner(System.in);
+	do{
+		int choice = 0;
+		
+			try{
+				mainMenu();
+				choice = sc.nextInt();
+				System.out.println(" ");
+				if (choice > 6 || choice < 0)
+				{
+					System.out.println("Error Number!!! Please choose 1 to 6.");
+				}else
+					{
+					switch(choice){
+					case 1:
+						subMenu1();
+						break;
+					case 2:
+						subMenu2();
+						break;
+					case 3:
+						subMenu3();
+						break;
+					case 4:
+						//get 比赛结果信息
+						break;
+					case 5:
+						//get 运动员得分
+						break;						
+								  }
+					}
+			     }catch(InputMismatchException i)
+			      {
+			    	 
+			      }
 			
-		}
+			
 	}
 
-}
+		//print main menu of the game
+		private void mainMenu(){
+			System.out.println("Ozlympic Game");
+			System.out.println("==================");
+			System.out.println("1. Select a game to run");
+			System.out.println("2. Predict the winner of the game");
+			System.out.println("3. Start the game");
+			System.out.println("4. Display the final results of all games");
+			System.out.println("5. Display the points of all athletes");
+			System.out.println("6. Exit");
+			System.out.println("");
+			System.out.println("Enter an option: ");
+		}
+		
+	}
+
+
