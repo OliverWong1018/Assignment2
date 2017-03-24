b64bf3c5af755629adce60c3fb886010ba5a2c04
rmit-s3577898-Wen-ZHang <s3577898@student.rmit.edu.au>
2017-03-24 23:05:34 +1100

    update

---
+++ AP - assignment1/src/SportsPreparing.java
@@ -0,0 +1,12 @@
+
+public class SportsPreparing {
+	public static Referee creatReferee(){
+		Referee rf = new Referee("R1", "Wayne", 31, "VIC");
+		return rf;
+	} 
+	public static Athlete[] creatAthletes(int amount){
+		Athlete ath1 = new Athlete("A1", "Tim", 31, "TAS", "Swimming");
+		return null;		
+	}
+	
+}
