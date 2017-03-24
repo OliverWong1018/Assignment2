62feb44a1ee59cd7163b8cd70c5941ad8c04b922
JINZE WANG <s3577346@student.rmit.edu.au>
2017-03-24 23:30:30 +1100

    updated

--- AP - assignment1/src/SportsPreparing.java
+++ AP - assignment1/src/SportsPreparing.java
@@ -6,6 +6,30 @@ public class SportsPreparing {
 	} 
 	public static Athlete[] creatAthletes(int amount){
 		Athlete ath1 = new Athlete("A1", "Tim", 31, "TAS", "Swimming");
+		Athlete ath2 = new Athlete("A2", "Oliver", 28, "WA", "Swimming" );
+		Athlete ath3 = new Athlete("A3", "Tony", 28, "QLD", "Swimming");
+		Athlete ath4 = new Athlete("A4", "Abby", 29, "NT", "Swimming");
+		Athlete ath5 = new Athlete("A5", "Sid", 25, "QLD", "Swimming");
+		Athlete ath6 = new Athlete("A6", "Danny", 26, "NSW", "Swimming");
+		Athlete ath7 = new Athlete("A7", "Barry", 23, "VIC", "Running");
+		Athlete ath8 = new Athlete("A8", "Allen", 24, "TAS", "Running");
+		Athlete ath9 = new Athlete("A9", "Dylan", 27, "QLD", "Running");
+		Athlete ath10 = new Athlete("A10", "Chris", 23, "WA", "Running");
+		Athlete ath11 = new Athlete("A11", "Jack", 27, "NT", "Running");
+		Athlete ath12 = new Athlete("A12", "Lon", 29, "VIC", "Running");
+		Athlete ath13 = new Athlete("A13", "Miya", 27, "QLD", "Cycling");
+		Athlete ath14 = new Athlete("A14", "Toby", 22, "TAS", "Cycling");
+		Athlete ath15 = new Athlete("A15", "Helvin", 26, "SA", "Cycling");
+		Athlete ath16 = new Athlete("A16", "Cara", 26, "VIC", "Cycling");
+		Athlete ath17 = new Athlete("A17", "Beryl", 25, "SA", "Cycling");
+		Athlete ath18 = new Athlete("A18", "Catherine", 25, "NSW", "Cycling");
+		Athlete ath19 = new Athlete("A19", "Michael", 25, "TAS", "superAthlete");
+		Athlete ath20 = new Athlete("A20", "Messi", 28, "SA", "superAthlete");
+		Athlete ath21 = new Athlete("A21", "Neymar", 24, "VIC", "superAthlete");
+		Athlete ath22 = new Athlete("A22", "Patto", 25, "NSW", "superAthlete");
+		Athlete ath23 = new Athlete("A23", "Jerry", 26, "VIC", "superAthlete");
+		Athlete ath24 = new Athlete("A24", "Rooney", 30, "TAS", "superAthlete");
+		
 		return null;		
 	}
 	
