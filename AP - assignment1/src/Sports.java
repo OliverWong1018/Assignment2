579d953acc8297cd3caf7c046b58dbbc8fb97b65
JINZE WANG <s3577346@student.rmit.edu.au>
2017-03-31 13:18:04 +1100

    updated comments

--- AP - assignment1/src/Sports.java
+++ AP - assignment1/src/Sports.java
@@ -9,27 +9,43 @@ public abstract class Sports {
 		this.setReferee(referee);
 		this.setAthletes(athletes);
 	}
-
+	
+	/*
+	 * get unique ID for each game.
+	 * @return
+	 */
 	public String getSportsID() {
 		return sportsID;
 	}
-
+	
+	/*
+	 * set unique ID for each game
+	 */
 	public void setSportsID(String sportsID) {
 		this.sportsID = sportsID;
 	}
-
+	
+	/*
+	 * get referee for each game
+	 * @return
+	 */
 	public Referee getReferee() {
 		return referee;
 	}
-
+	
+	/*
+	 * set referee for each game
+	 */
 	public void setReferee(Referee referee) {
 		this.referee = referee;
 	}
-
+	
+	// get athletes for each game
 	public Athlete[] getAthletes() {
 		return athletes;
 	}
-
+	
+	// set athlete for each game
 	public void setAthletes(Athlete[] athletes) {
 		this.athletes = athletes;
 	}
