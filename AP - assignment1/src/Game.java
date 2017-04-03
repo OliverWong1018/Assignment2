79c697b4cbe56ad5ac61bcfe79161bd8109ef2f2
rmit-s3577898-Wen-ZHang <s3577898@student.rmit.edu.au>
2017-04-03 13:02:15 +1000

    update

--- AP - assignment1/src/Game.java
+++
@@ -1,8 +0,0 @@
-
-public abstract class Game {
-
-	public Game(){
-		
-	}
-
-}
--- AP - assignment1/src/Referee.java
+++ AP - assignment1/src/Referee.java
@@ -6,7 +6,5 @@ public class Referee extends Participant{
 		// TODO Auto-generated constructor stub
 	}
 	//Referee can summarise the score each game
-	public int summaries(Game g){
-		return 0 ;
-	}
+	
 }
