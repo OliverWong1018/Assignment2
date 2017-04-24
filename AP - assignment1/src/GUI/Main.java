e748b6031c61d382bc8e55081edf8049a6f49d1d
JINZE WANG <s3577346@student.rmit.edu.au>
2017-04-24 22:15:11 +1000

    Updated GUI

--- AP - assignment1/src/GUI/Main.java
+++ AP - assignment1/src/GUI/Main.java
@@ -10,23 +10,31 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	private Stage primaryStage;
-	private AnchorPane mainLayout;
+	private static AnchorPane mainLayout;
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Ozlympic");
-		showMainView();
+		showWelcomeView();
 
 	}
 
-	private void showMainView() throws IOException{
+	private void showWelcomeView() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("/fxml/Welcome.fxml"));
 		mainLayout = loader.load();
 		Scene scene = new Scene(mainLayout);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	public static void showMainPageScene() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("/fxml/MainPage.fxml"));
+		AnchorPane mainPage = loader.load();
+		mainLayout.setCenter(mainPage);
+		
 	}
 
 	public static void main(String[] args) {
---
+++ AP - assignment1/src/GUI/MainController.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+	
+	private Main main;
+	
+	@FXML
+	private void goMainPage() throws IOException{
+		main.showMainPageScene();
+	}
+
+}
