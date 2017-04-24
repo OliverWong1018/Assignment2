f1cce35b9f31a0fcd18f64957b0f54d0ad0aa292
JINZE WANG <s3577346@student.rmit.edu.au>
2017-04-24 22:57:21 +1000

    Updated GUI

--- AP - assignment1/src/GUI/Main.java
+++ AP - assignment1/src/GUI/Main.java
@@ -6,11 +6,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private Stage primaryStage;
-	private static AnchorPane mainLayout;
+	private static BorderPane mainLayout;
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
@@ -22,7 +23,7 @@ public class Main extends Application {
 
 	private void showWelcomeView() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("/fxml/Welcome.fxml"));
+		loader.setLocation(Main.class.getResource("/GUI/Welcome.fxml"));
 		mainLayout = loader.load();
 		Scene scene = new Scene(mainLayout);
 		primaryStage.setScene(scene);
@@ -31,10 +32,9 @@ public class Main extends Application {
 	
 	public static void showMainPageScene() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("/fxml/MainPage.fxml"));
-		AnchorPane mainPage = loader.load();
-		mainLayout.setCenter(mainPage);
-		
+		loader.setLocation(Main.class.getResource("/GUI/MainPage.fxml"));
+		BorderPane mainPage = loader.load();
+	    mainLayout.setCenter(mainPage);
 	}
 
 	public static void main(String[] args) {
