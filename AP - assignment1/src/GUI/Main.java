d01cc75a98d31f499d74ae55f2d75bb2ff5694dd
JINZE WANG <s3577346@student.rmit.edu.au>
2017-05-04 19:53:08 +1000

    Updated GUI

--- AP - assignment1/src/GUI/Main.java
+++ AP - assignment1/src/GUI/Main.java
@@ -37,7 +37,7 @@ public class Main extends Application {
 		primaryStage.show();
 		primaryStage.setTitle("Ozlympic");
 	}
-	
+
 	public static void displayResult() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("/GUI/AllResults.fxml"));
