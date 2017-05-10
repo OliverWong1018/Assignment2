64c7b844e150f5968c71ebd31ab232def0460996
JINZE WANG <s3577346@student.rmit.edu.au>
2017-05-10 20:01:19 +1000

    Updated

--- AP - assignment1/src/GUI/Table.java
+++ AP - assignment1/src/GUI/Table.java
@@ -3,27 +3,30 @@ package GUI;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Table {
-	private SimpleStringProperty rType1;
+	private SimpleStringProperty rID1;
 	private SimpleStringProperty rName1;
-	private SimpleStringProperty rAthleteType1;
+	private SimpleStringProperty rAge1;
+	private SimpleStringProperty rState1;
+	private SimpleStringProperty rType1;
+	
 
-	public Table(String rType1, String rName1, String rAthleteType1) {
-		this.rType1=(new SimpleStringProperty(rType1));
+	public Table(String rID1, String rName1, String rAge1, String rState1, String rType1) {
+		this.rID1=(new SimpleStringProperty(rID1));
 		this.rName1 = new SimpleStringProperty(rName1);
-		this.rAthleteType1 = new SimpleStringProperty(rAthleteType1);
+		this.rAge1 = new SimpleStringProperty(rAge1);
+		this.rState1 = new SimpleStringProperty(rState1);
+		this.rType1 = new SimpleStringProperty(rType1);
 
 	}
 
-	public Table() {
-		// TODO Auto-generated constructor stub
+	
+
+	public String getRID1() {
+		return rID1.get();
 	}
 
-	public String getRType1() {
-		return rType1.get();
-	}
-
-	public void setRType1(String v) {
-		rType1.set(v);
+	public void setRID1(String v) {
+		rID1.set(v);
 	}
 
 	public String getRName1() {
@@ -34,12 +37,28 @@ public class Table {
 		rName1.set(v);
 	}
 
-	public String getRAthleteType1() {
-		return rAthleteType1.get();
+	public String getRAge1() {
+		return rAge1.get();
 	}
 
-	public void setRAthleteType1(String v) {
-		rAthleteType1.set(v);
+	public void setRAge1(String v) {
+		rAge1.set(v);
+	}
+	
+	public String getRState1() {
+		return rState1.get();
+	}
+
+	public void setRState1(String v) {
+		rState1.set(v);
+	}
+	
+	public String getRType1() {
+		return rType1.get();
+	}
+
+	public void setRType1(String v) {
+		rType1.set(v);
 	}
 
 }
---
+++ AP - assignment1/src/GUI/Table3.java
@@ -0,0 +1,44 @@
+/*package GUI;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Table3 {
+	private SimpleStringProperty rRank3;
+	private SimpleStringProperty rName3;
+	private SimpleStringProperty rTime3;
+	private SimpleStringProperty rPoints3;
+
+	public Table3(String rRank3, String rName3, String rTime3, String rPonits3) {
+		this.rRank3=new SimpleStringProperty(rRank3);
+		this.rName3 = new SimpleStringProperty(rName3);
+		this.rTime3 = new SimpleStringProperty(rTime3);
+		this.rPoints3 = new SimpleStringProperty(rPoints3);
+
+	}
+
+	public String getRRank3() {
+		return rRank3.get();
+	}
+
+	public void setRank3(String v) {
+		rRank3.set(v);
+	}
+
+	public String getRName3() {
+		return rName3.get();
+	}
+
+	public void setRName3(String v) {
+		rName3.set(v);
+	}
+
+	public String getRAthleteType3() {
+		return rAthleteType3.get();
+	}
+
+	public void setRAthleteType3(String v) {
+		rAthleteType3.set(v);
+	}
+
+}
+*/
