42b9ad3e3529455a987f694c7eea10b2e68d67f8
rmit-s3577898-Wen-ZHang <s3577898@student.rmit.edu.au>
2017-05-17 07:10:03 +1000

    up

--- AP - assignment1/src/MyExceptions/DBException.java
+++ AP - assignment1/src/MyExceptions/DBException.java
@@ -1,5 +1,11 @@
 package MyExceptions;
 
-public class DBException {
+import javafx.scene.control.Label;
+
+public class DBException extends Exception {
 	
-}
+	   public DBException(String errMsg, Label notice) {
+	      super(errMsg); 
+	      notice.setText("The DataBase connection doesn't work well now, and TXT will work instead");
+	   } 
+	}
--- AP - assignment1/src/MyExceptions/TXTException.java
+++ AP - assignment1/src/MyExceptions/TXTException.java
@@ -1,5 +1,11 @@
 package MyExceptions;
 
-public class TXTException {
+import javafx.scene.control.Label;
 
-}
+public class TXTException extends Exception {
+	
+	   public TXTException(String errMsg, Label notice) {
+	      super(errMsg); 
+	      notice.setText("The TXT connection doesn't work well now, and Database will work instead");
+	   } 
+	}
